6243359c385d759ee53164e6681b726b2ccfd86e
anita <asovay@gmail.com>
2017-01-06 16:53:07 -0500

    vid 4

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import Main from '../components/Main';
+import Home from '../components/Home';
+
+export const routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+);
