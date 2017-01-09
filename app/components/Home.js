7d01c7fe00e44f3dcc80a97d50ccbcfb91e5ffde
anita <asovay@gmail.com>
2017-01-09 15:54:46 -0500

    vid 5

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 export class Home extends React.Component {
   render() {
     return (
-      <div>
-        Hello from Home!
+      <div className='jumbotron col-sm-12 text-center'>
+        <h1>Github Battle</h1>
+        <p className='lead'>Some fancy motto</p>
+        <Link to='/playerOne'>
+        	<button type='button' className='btn btn-lg btn-success'>
+        		Get Started
+        	</button>
+        </Link>
       </div>
     )
   }
