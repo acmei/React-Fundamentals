2f3b526054cc3bc5c140586eead2f74f6e84db93
anita <asovay@gmail.com>
2017-01-10 15:51:12 -0500

    add transitions and finish tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] }},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
     ]
   },
   plugins: [HTMLWebpackPluginConfig]
